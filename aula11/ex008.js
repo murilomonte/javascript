e0102df556de8b8e57e20c5963cfe7405ee656a1
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2022-08-01 16:55:25 -0300

    aula11 created

---
+++ aula11/ex008.js
@@ -0,0 +1,18 @@
+/*
+var vel = 59.9
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60) {
+    console.log('você ultrapassou a velocidade permitida. Multado')
+}
+
+console.log('dirija com cuidado')
+*/
+
+var pais = 'EUA'
+if (pais == "BRASIL") {
+    console.log('brasileiro')
+} else {
+    console.log('estrangeiro')
+}
