ea1a8808e547b7c06c129491966d6f8806238e5b
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2022-08-06 17:53:22 -0300

    primeiro exercício finalizado

--- exercicios/aula12ex/ex014/script.js
+++ exercicios/aula12ex/ex014/script.js
@@ -1,14 +1,20 @@
 let dia = new Date()
 let diaHora = dia.getHours()
 
-let divImg = document.getElementById('div-imagem')
-let divHora = document.getElementById('div-horario')
+let divImg = document.getElementById('imagem')
+let divHora = document.getElementById('horario')
 
 divHora.innerHTML = `<p>Agora são ${diaHora} horas`
 if (diaHora < 12) {
-    divImg.innerHTML = `<img src="teste.jpeg">`
+    //#4d5e2a
+    document.body.style = `background-color: #4d5e2a;`
+    divImg.src = `foto-manha.jpg`
 } else if (diaHora < 18) {
-    
+    //#fdcb98
+    divImg.src = `foto-tarde.jpg`
+    document.body.style = `background-color: #fdcb98;`
 } else {
-
+    //#997eb5
+    divImg.src = `foto-noite.jpg`
+    document.body.style = `background-color: #997eb5;`
 }
