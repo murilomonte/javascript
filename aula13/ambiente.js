b3ad01066205f77d31916caaf7db4f306186824b
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2022-08-10 08:29:27 -0300

    some changes

---
+++ aula13/ambiente.js
@@ -0,0 +1,12 @@
+let contador = 1
+/*
+while (contador <= 6) {
+    console.log(`passo ${contador}`)
+    contador++
+}*/
+
+do {
+    console.log(`passo ${contador}`)
+    contador++
+} while (contador <= 6)
+
