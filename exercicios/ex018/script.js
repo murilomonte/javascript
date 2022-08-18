554851617b84421c5b8ce7c496686c69cea8c9d4
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2022-08-18 19:06:15 -0300

    inicio do exercício

---
+++ exercicios/ex018/script.js
@@ -0,0 +1,16 @@
+function contabilizar() {
+    console.log('poggers')
+    let num = Number(document.getElementById('inum').value)
+    console.log(num)
+    return num
+}
+
+function analisar() {
+    console.log
+    let log = document.getElementById('ilog')
+    let res = document.getElementById('div-res')
+
+    if (num.length == undefined) {
+        window.alert('Adicione valores antes de finalizar')
+    } 
+}
