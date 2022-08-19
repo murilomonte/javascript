2fae9743acd018db990d48de66d8929dcd60105c
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2022-08-19 12:14:09 -0300

    exercício finalizado

--- exercicios/ex018/script.js
+++ exercicios/ex018/script.js
@@ -1,16 +1,49 @@
-function contabilizar() {
-    console.log('poggers')
+let dados = []
+console.log(dados.length)
+let ilog = document.getElementById('ilog')
+let res = document.getElementById('div-res')
+
+
+function conta() {
     let num = Number(document.getElementById('inum').value)
     console.log(num)
-    return num
+    let log = document.createElement('option')
+    let busca = dados.indexOf(num)
+    console.log(busca)
+
+    if (num <= 0 || num > 100) {
+        window.alert('valor inválido')
+    } else if (dados.length > 0 && busca > -1) {
+        window.alert(`o valor ${num} já está na lista`)
+    } else {
+        res.innerHTML = ''
+        log.innerText = `o número ${num} foi adicionado.`
+        ilog.appendChild(log)
+    
+        return dados.push(num)
+    }
 }
 
-function analisar() {
-    console.log
-    let log = document.getElementById('ilog')
-    let res = document.getElementById('div-res')
-
-    if (num.length == undefined) {
+function fina() {
+    if (dados.length == 0) {
         window.alert('Adicione valores antes de finalizar')
-    } 
+    } else {
+        //peguei do w3schools
+        function compareFunction(a, b){return a-b}
+        
+        dados.sort(compareFunction)
+        let tam = dados.length - 1
+
+        res.innerHTML = `<p>Ao todo, temos ${dados.length} números cadastrados. </p>`
+        res.innerHTML += `<p>O maior valor informado é ${dados[tam]}</p>`
+        res.innerHTML += `<p>O menor valor informado é ${dados[0]}</p>`
+    
+        let soma = 0
+        for (let pos = 0; pos < dados.length; pos++) {
+            soma += dados[pos]
+        }
+
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${soma / dados.length}</p>`
+        }
 }
